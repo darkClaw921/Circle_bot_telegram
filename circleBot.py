61b7040c01e4f372f852b89faf7f2aced3e5f4cb
Igor <igor.gerasimov.98@icloud.com>
2022-10-14 16:51:52 +0300

    работает со всеми разрешениями

--- circleBot.py
+++ circleBot.py
@@ -12,7 +12,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import os
 
-logger.add('log.log', level='INFO')
+logger.add('log.log', level='DEBUG')
 
 load_dotenv()
 tgToken = os.environ.get('TG_TOKEN')
@@ -27,8 +27,25 @@ def test_movie(path):
     clip = mpy.VideoFileClip(f'{path}')
     (w, h) = clip.size
     #720 не работает
-    w = 640
-    os.system(f'ffmpeg -i {path} -filter:v "crop={w}:{w}:{0}:{h/4}" 0{path}')
+    #w = 640
+    if w > h:
+        if w > 640:
+            w = 640
+        os.system(f'ffmpeg -i {path} -filter:v "crop={h}:{h}:{w/4}:{0}" 0{path}')
+        logger.debug(f'левый угол {w/4} {0}')
+        logger.debug(f'{w} {h}')
+    else:  
+        if w > 640:
+            w=640
+        os.system(f'ffmpeg -i {path} -filter:v "crop={w}:{w}:{0}:{h/4}" 0{path}')
+        logger.debug(f'левый угол {0} {h/4}')
+        logger.debug(f'{w} {h}')
+                
+    
+    #os.system(f'ffmpeg -i {path} -filter:v "crop={w}:{w}:{0}:{h/4}" 0{path}')
+    #os.system(f'ffmpeg -i {path} -filter:v "crop={w}:{w}:{0}:{h/4}" -c:v mp4 -vtag XVID -q:v 4 -c:a libmp3lame -q:a 4 0{path}')
+#-c:v mpeg4 -vtag XVID -q:v 4 -c:a libmp3lame -q:a 4
+#    -c:v libx264 -preset slow -crf 0
     #когда нибуди добавить статистику
 
     #a = os.system(f'du -h 0{path}')
@@ -38,11 +55,20 @@ def test_movie(path):
 @logger.catch
 async def handle_docs_photo(message):
     name = message.from_user.id
+    #try:
     #firstName = message.first_name
     #logger.info(f'{firstName} ')
-    await message.video.download(f'{name}.mp4')
+    file_id = message.video.file_id # Get file id
+    file1 = await bot.get_file(file_id) # Get file path
+    await bot.download_file(file1.file_path, f"{name}.mp4") # Download video and save output in file "video.mp4"
+    #await message.video.download(f'{name}.mp4')
     test_movie(f'{name}.mp4')
-    await bot.send_video_note(name, video_note= InputFile(f'0{name}.mp4'),duration=2)#open(f'0{name}.mp4', 'rb'))#f'{name}.mp4')#f'{name}.png')
+    files = '/home/igor/python/igor2/venvcircle_bot/Circle_bot_telegram'
+    await bot.send_video_note(name, video_note= InputFile(f'{files}/0{name}.mp4'))#,duration=2)#open(f'0{name}.mp4', 'rb'))#f'{name}.mp4')#f'{name}.png')
+    #except Exception as e:
+    logger.info('скачиваем файл {e}')
+
+    os.system(f'rm {name}.mp4')
     os.system(f'rm 0{name}.mp4')
 
 @dp.message_handler(content_types=ContentType.ANY)
@@ -50,8 +76,11 @@ async def handle_docs_photo(message):
 async def echo_message(msg: types.Message):
     state = dp.current_state(user=msg.from_user.id)
     
-    message = msg.text.lower()
-    user_id = msg.from_user.id
+    try:
+        message = msg.text.lower()
+        user_id = msg.from_user.id
+    except Exception as e:
+        logger.info(f'любой формат {e}')
 
     await bot.send_message(msg.from_user.id, 'Пришлите ваше видео')
     #await state.set_state('firstMsg')
